55f0b6595162d2087b2e16614da5bad5d2948843
Tham Cheng Yong <tcy604@hotmail.com>
2017-11-24 17:32:44 +0800

    Update jcsongs.R

--- jcsongs.R
+++ jcsongs.R
@@ -1,19 +1,21 @@
 readinteger <- function()
 { 
-  n <- readline(prompt=paste('Choose song:','\n','1: Gao Bai Qi Qiu','\n','2: Bu Gai','\n'))
+  n <- readline(prompt=paste('Choose song:','\n','1: Gao Bai Qi Qiu','\n','2: Bu Gai','\n','3: Other songs','\n','0: Exit','\n'))
   
   n <- as.integer(n)
   if (is.na(n)){
     n <- readinteger()
   }
-  if (n==1)
+  while(TRUE) {
+  if (n==0) return("Stopped")
+  else if (n==1)
     browseURL('https://www.youtube.com/watch?v=bu7nU9Mhpyo', browser = getOption("browser"),
           encodeIfNeeded = FALSE)
   else if (n==2)
     browseURL('https://www.youtube.com/watch?v=_VxLOj3TB5k', browser = getOption("browser"),
               encodeIfNeeded = FALSE)
 
-  n <- readline(prompt=paste('Any more songs?','\n'))
+  n <- readline(prompt=paste('Song name?','\n'))
   n = gsub(" ","+",n)
   thepage = readLines(paste('https://www.youtube.com/results?search_query=',n,sep=""))
   lockupcontent = thepage[grep('yt-lockup-content',thepage)[1]]
@@ -23,7 +25,7 @@ readinteger <- function()
   string = substring(lockupcontent,goodlines1,goodlines2)
   browseURL(paste('https://www.youtube.com',string,sep=''), browser = getOption("browser"),
             encodeIfNeeded = FALSE)
-}
+}}
 
 
 
